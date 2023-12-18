ea637982ae40b9dee376e571244bcc0f38b2beb6
Marco Soares <106749775+Sengeki1@users.noreply.github.com>
2023-12-18 16:12:30 -0100

    joined all df

--- Data Mining/analysis.py
+++ Data Mining/analysis.py
@@ -12,13 +12,18 @@ for file in os.listdir(data_file_folder):
         file_name = os.path.splitext(file) # tuple(name, after dot)
         dfs[file_name[0]] = df
 
-merged_df = pd.merge(dfs['client'], dfs['disp'], on='client_id', how='inner')
-merged_df = pd.merge(merged_df, dfs['account'], on='account_id', how='inner') 
-merged_df = pd.merge(merged_df, dfs['order'], on='account_id', how='inner')  
-merged_df = pd.merge(merged_df, dfs['trans'], on='account_id', how='inner')  
-merged_df = pd.merge(merged_df, dfs['loan'], on='account_id', how='inner')  
-merged_df = pd.merge(merged_df, dfs['card'], on='account_id', how='inner')  
-merged_df = pd.merge(merged_df, dfs['district'], left_on='district_id_x', right_on='A1', how='inner')
-print("...Data merged!!")
+merged_df = pd.merge(dfs['client'], dfs['disp'], on='client_id', how='inner') # relation client & disposition 
 
+dfs['district'] = dfs['district'].rename(columns ={'A1': 'district_id'})
+merged_df = pd.merge(merged_df, dfs['district'], on='district_id', how='inner') # relation between the current df & demograph
+merged_df = merged_df.sort_values(['client_id'], ascending=True)
+
+merged_df = pd.merge(merged_df, dfs['account'], on=['district_id', 'account_id'], how='inner') # relation between account & current df
+merged_df = pd.merge(merged_df, dfs['card'], on='disp_id', how='inner') # relation between card & current df
+
+merged_df = pd.merge(merged_df, dfs['loan'], on='account_id', how='inner') # relation between df & loan
+merged_df = pd.merge(merged_df, dfs['order'], on='account_id', how='inner') # relation between df & order
+merged_df = pd.merge(merged_df, dfs['trans'], on='account_id', how='inner') # relation between df & trans
+
+print("...Data merged!!")
 merged_df.to_csv('merged_data.csv', index=False)
